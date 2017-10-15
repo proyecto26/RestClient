dc0cb20739779f8e6ab42e9b89d718887dbe9cf4
Juan David Nicholls Cardona <jdnichollsc@hotmail.com>
2017-10-15 02:05:16 -0500

    Update demo

--- demo/Assets/MainScript.cs
+++ demo/Assets/MainScript.cs
@@ -1,27 +1,28 @@
 ﻿using UnityEngine;
 using UnityEditor;
 using Models;
+using Proyecto26.RestClient;
 
 public class MainScript : MonoBehaviour {
 
-	//string basePath = "https://jsonplaceholder.typicode.com";
+	string basePath = "https://jsonplaceholder.typicode.com";
 
 	public void Get(){
 
-        /*RestClient.GetArray<Post>(basePath + "/posts").Then(res => {
-			EditorUtility.DisplayDialog ("Success", JsonHelper.ArrayToJson<Post>(res, true), "Ok");
-			return RequestHelper.GetArray<Todo>(basePath + "/todos");
+        RestClient.GetArray<Post>(basePath + "/posts").Then(res => {
+            EditorUtility.DisplayDialog ("Success", JsonHelper.ArrayToJsonString<Post>(res, true), "Ok");
+            return RestClient.GetArray<Todo>(basePath + "/todos");
 		}).Then(res => {
-			EditorUtility.DisplayDialog ("Success", JsonHelper.ArrayToJson<Todo>(res, true), "Ok");
-			return RequestHelper.GetArray<User>(basePath + "/users");
+            EditorUtility.DisplayDialog ("Success", JsonHelper.ArrayToJsonString<Todo>(res, true), "Ok");
+            return RestClient.GetArray<User>(basePath + "/users");
 		}).Then(res => {
-			EditorUtility.DisplayDialog ("Success", JsonHelper.ArrayToJson<User>(res, true), "Ok");
-		}).Catch(err => EditorUtility.DisplayDialog ("Error", err.Message, "Ok"));*/
+            EditorUtility.DisplayDialog ("Success", JsonHelper.ArrayToJsonString<User>(res, true), "Ok");
+		}).Catch(err => EditorUtility.DisplayDialog ("Error", err.Message, "Ok"));
 	}
 
 	public void Post(){
 		
-		/*RestClient.Post<Models.Post>(basePath + "/posts", new {
+		RestClient.Post<Models.Post>(basePath + "/posts", new {
 			title = "foo",
 			body = "bar",
 			userId = 1
@@ -32,12 +33,12 @@ public class MainScript : MonoBehaviour {
 			else{
 				EditorUtility.DisplayDialog ("Success", JsonUtility.ToJson(body, true), "Ok");
 			}
-		});*/
+		});
 	}
 
 	public void Put(){
 
-		/*RequestHelper.Put<Post>(basePath + "/posts/1", new {
+        RestClient.Put<Post>(basePath + "/posts/1", new {
 			title = "foo",
 			body = "bar",
 			userId = 1
@@ -48,18 +49,18 @@ public class MainScript : MonoBehaviour {
 			else{
 				EditorUtility.DisplayDialog ("Success", JsonUtility.ToJson(body, true), "Ok");
 			}
-		});*/
+		});
 	}
 
 	public void Delete(){
 
-		/*RequestHelper.Delete(basePath + "/posts/1", (err, res) => {
+        RestClient.Delete(basePath + "/posts/1", (err, res) => {
 			if(err != null){
 				EditorUtility.DisplayDialog ("Error", err.Message, "Ok");
 			}
 			else{
 				EditorUtility.DisplayDialog ("Success", "Status: " + res.statusCode.ToString(), "Ok");
 			}
-		});*/
+		});
 	}
 }
