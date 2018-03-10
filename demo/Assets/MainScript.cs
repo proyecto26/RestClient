7c6ec990d2795d8618fd9a0f81f09b39c18b93d6
Juan David Nicholls Cardona <jdnichollsc@hotmail.com>
2018-03-10 13:53:51 -0500

    Fix issue building demo

--- demo/Assets/MainScript.cs
+++ demo/Assets/MainScript.cs
@@ -13,7 +13,7 @@ public class MainScript : MonoBehaviour {
 		// We can add default request headers for all requests
 		RestClient.DefaultRequestHeaders["Authorization"] = "Bearer ...";
 
-		RequestHelper requestOptions;
+		RequestHelper requestOptions = null;
 
 		RestClient.GetArray<Post>(basePath + "/posts").Then(res => {
 			EditorUtility.DisplayDialog ("Posts", JsonHelper.ArrayToJsonString<Post>(res, true), "Ok");
