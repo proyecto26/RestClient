ee385b675a16f1c332d339e0bcf0a34939f97dbd
Juan David Nicholls Cardona <jdnichollsc@hotmail.com>
2019-04-19 19:52:58 -0500

    Add try catch to prevent issues parsing JSON with JsonUtility

--- src/Proyecto26.RestClient/RestClient.cs
+++ src/Proyecto26.RestClient/RestClient.cs
@@ -9,7 +9,7 @@ namespace Proyecto26
 {
     /// <summary>
     /// RestClient for Unity
-    /// Version: 2.5.4
+    /// Version: 2.5.5
     /// </summary>
     public static partial class RestClient
     {
