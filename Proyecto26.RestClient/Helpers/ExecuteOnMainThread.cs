6afe4d110972725c6ef389874bbc55cb19587185
Juan David Nicholls Cardona <jdnichollsc@hotmail.com>
2021-12-09 15:41:59 -0500

    Code review

--- Proyecto26.RestClient/Helpers/ExecuteOnMainThread.cs
+++ Proyecto26.RestClient/Helpers/ExecuteOnMainThread.cs
@@ -1,36 +1,40 @@
 using System;
-using System.Collections.Generic;
 using System.Collections.Concurrent;
 using UnityEngine;
 
-namespace Proyecto26.Helper {
-
-  /// <summary>
-  /// Original solution from: https://stackoverflow.com/a/53818416/10305444
-  /// Should also read: https://gamedev.stackexchange.com/a/195298/126092
-  /// Usage:
-  /// ```
-  ///   ExecuteOnMainThread.RunOnMainThread.Enqueue(() => {
-  ///     Code here will be called in the main thread...
-  ///   });
-  /// ```
-  /// </summary>
-  public class ExecuteOnMainThread: MonoBehaviour {
-
+namespace Proyecto26.Helper
+{
     /// <summary>
-    /// Store all instance of Action's and try to invoke them
+    /// Original solution from: https://stackoverflow.com/a/53818416/10305444
+    /// Should also read: https://gamedev.stackexchange.com/a/195298/126092
     /// </summary>
-    public static readonly ConcurrentQueue < Action > RunOnMainThread = new ConcurrentQueue < Action > ();
+    /// <example>
+    /// Code here will be called in the main thread.
+    /// <code>
+    /// ExecuteOnMainThread.RunOnMainThread.Enqueue(() => {
+    ///     // Any API call using RestClient
+    /// });
+    /// </code>
+    /// </example>
+    public class ExecuteOnMainThread : MonoBehaviour
+    {
+        /// <summary>
+        /// Store all instance of Action's and try to invoke them
+        /// </summary>
+        public static readonly ConcurrentQueue<Action> RunOnMainThread = new ConcurrentQueue<Action>();
 
-    /// <summary>
-    /// Unity's Update method
-    /// </summary>
-    void Update() {
-      if (!RunOnMainThread.IsEmpty) {
-        while (RunOnMainThread.TryDequeue(out var action)) {
-          action?.Invoke();
+        /// <summary>
+        /// Unity's Update method
+        /// </summary>
+        void Update()
+        {
+            if (!RunOnMainThread.IsEmpty)
+            {
+                while (RunOnMainThread.TryDequeue(out var action))
+                {
+                    action?.Invoke();
+                }
+            }
         }
-      }
     }
-  }
 }
